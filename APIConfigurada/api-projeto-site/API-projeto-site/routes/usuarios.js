f2f5fddc29f186c720e4791f8b0e470228098a0b
gui0103 <guilherme.ccosta@bandtec.com.br>
2021-11-19 00:49:27 -0300

    upgrade seleção de componentes funcional

--- APIConfigurada/api-projeto-site/API-projeto-site/routes/usuarios.js
+++ APIConfigurada/api-projeto-site/API-projeto-site/routes/usuarios.js
@@ -6,12 +6,12 @@ var Usuario = require('../models').Usuario;
 let sessoes = [];
 
 /* Recuperar usuário por login e senha */
-router.post('/autenticar', function(req, res, next) {
+router.post('/autenticar', function (req, res, next) {
 	console.log('Recuperando usuário por login e senha');
 
 	var email = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
-	
+
 	let instrucaoSql = `select * from Funcionario where email='${email}' and senha='${senha}'`;
 	console.log(instrucaoSql);
 
@@ -22,7 +22,7 @@ router.post('/autenticar', function(req, res, next) {
 
 		if (resultado.length == 1) {
 			sessoes.push(resultado[0].dataValues.email);
-			console.log('sessoes: ',sessoes);
+			console.log('sessoes: ', sessoes);
 			res.json(resultado[0]);
 		} else if (resultado.length == 0) {
 			res.status(403).send('Login e/ou senha inválido(s)');
@@ -33,13 +33,13 @@ router.post('/autenticar', function(req, res, next) {
 	}).catch(erro => {
 		console.error(erro);
 		res.status(500).send(erro.message);
-  	});
+	});
 });
 
 /* Cadastrar usuário */
-router.post('/cadastrar', function(req, res, next) {
+router.post('/cadastrar', function (req, res, next) {
 	console.log('Criando um usuário');
-	
+
 	Usuario.create({
 		nome: req.body.nome,
 		cargaHoraria: req.body.cargaHoraria,
@@ -49,21 +49,21 @@ router.post('/cadastrar', function(req, res, next) {
 		senha: req.body.senha
 	}).then(resultado => {
 		console.log(`Registro criado: ${resultado}`)
-        res.send(resultado);
-    }).catch(erro => {
+		res.send(resultado);
+	}).catch(erro => {
 		console.error(erro);
 		res.status(500).send(erro.message);
-  	});
+	});
 });
 
 
 /* Verificação de usuário */
-router.get('/sessao/:login', function(req, res, next) {
+router.get('/sessao/:login', function (req, res, next) {
 	let login = req.params.login;
 	console.log(`Verificando se o usuário ${login} tem sessão`);
-	
+
 	let tem_sessao = false;
-	for (let u=0; u<sessoes.length; u++) {
+	for (let u = 0; u < sessoes.length; u++) {
 		if (sessoes[u] == login) {
 			tem_sessao = true;
 			break;
@@ -77,15 +77,15 @@ router.get('/sessao/:login', function(req, res, next) {
 	} else {
 		res.sendStatus(403);
 	}
-	
+
 });
 
 /* Logoff de usuário */
-router.get('/sair/:login', function(req, res, next) {
+router.get('/sair/:login', function (req, res, next) {
 	let login = req.params.login;
 	console.log(`Finalizando a sessão do usuário ${login}`);
 	let nova_sessoes = []
-	for (let u=0; u<sessoes.length; u++) {
+	for (let u = 0; u < sessoes.length; u++) {
 		if (sessoes[u] != login) {
 			nova_sessoes.push(sessoes[u]);
 		}
@@ -96,7 +96,7 @@ router.get('/sair/:login', function(req, res, next) {
 
 
 /* Recuperar todos os usuários */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	console.log('Recuperando todos os usuários');
 	Usuario.findAndCountAll().then(resultado => {
 		console.log(`${resultado.count} registros`);
@@ -105,7 +105,7 @@ router.get('/', function(req, res, next) {
 	}).catch(erro => {
 		console.error(erro);
 		res.status(500).send(erro.message);
-  	});
+	});
 });
 
 /* Listar funcionários */
@@ -114,6 +114,33 @@ router.post('/verOnline/:idFuncionario', function (req, res, next) {
 	let idFuncionario = req.params.idFuncionario;
 
 	let instrucaoSql = `select * from Funcionario where fk_Chefe='${idFuncionario}'`;
+	console.log(instrucaoSql);
+
+	sequelize.query(instrucaoSql, {
+		type: sequelize.QueryTypes.SELECT // se não for usar models
+	}).then(resultado => {
+
+		if (resultado.length > 0) {
+			res.json(resultado); // transforma resposta em json
+		} else {
+			res.status(403).send('sem funcionários registrados');
+		}
+
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+	});
+});
+
+/* Trazer componentes */
+router.post('/trazerComponentes/:idFuncionario', function (req, res, next) {
+
+	let idFuncionario = req.params.idFuncionario;
+
+	let instrucaoSql = `select nomeComponente from Componente cpt 
+	join Maquina mka on cpt.fk_Maquina = mka.idMaquina 
+	join Funcionario fc on mka.fk_Funcionario = '${idFuncionario}' group by nomeComponente`;
+
 	console.log(instrucaoSql);
 
 	sequelize.query(instrucaoSql, {
