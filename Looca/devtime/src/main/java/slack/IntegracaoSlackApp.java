9657b32d27eacf8a99782032eb9436b164e086e0
danilo199907 <danilo.santos01@Outlook.com>
2021-11-21 14:51:43 -0300

    Chat slack funcionando

---
+++ Looca/devtime/src/main/java/slack/IntegracaoSlackApp.java
@@ -0,0 +1,11 @@
+package slack;
+
+public class IntegracaoSlackApp {
+    
+    public static void main(String[] args) {
+        
+        IntegracaoSlack integracao = new IntegracaoSlack();
+        
+        integracao.enviarMensagem("SALVE RAPAZIADA, BOM FIM DE SEMANA A TODOS!");
+    }
+}
